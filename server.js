3382cb3a282f05ec8599c7de25a0bd99294b54a0
JTConnolly82 <47802786+JTConnolly82@users.noreply.github.com>
2019-08-08 14:37:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port}.`));
 
 
